923de91d0aa8a4f6acb28bd83464d49c501e5c44
KhaeeWinterfeld <wkhaee@gmail.com>
2022-12-01 14:31:44 -0300

    first commit

---
+++ projetovendas/src/main/java/br/univille/projetovendas/ProjetovendasApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetovendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetovendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetovendasApplication.class, args);
+	}
+
+}
---
+++ projetovendas/src/test/java/br/univille/projetovendas/ProjetovendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
